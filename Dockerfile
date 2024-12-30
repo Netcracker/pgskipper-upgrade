d09f38bf3ad90b4b00171a86f435e9590ad279f4
Tvion <snakersu@gmail.com>
2024-12-30 17:35:55 +0500

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,68 @@
+FROM ubuntu:22.04
+
+USER root
+
+ENV LC_ALL=en_US.UTF-8 \
+    LANG=en_US.UTF-8
+ARG DEBIAN_FRONTEND=noninteractive
+
+RUN echo start
+RUN mkdir -p /var/lib/pgsql/data/ && mkdir -p /var/lib/pgsql/tmp_data/
+
+# fix for non-ha
+RUN mkdir /properties && touch /properties/empty-conf.conf
+
+RUN echo "deb [trusted=yes] http://apt.postgresql.org/pub/repos/apt jammy-pgdg main" >> /etc/apt/sources.list.d/pgdg.list
+RUN ls -la /etc/apt/
+RUN apt-get -y update
+RUN apt-get -o DPkg::Options::="--force-confnew" -y dist-upgrade
+
+RUN groupmod -n postgres tape
+RUN adduser -uid 26 -gid 26 postgres
+
+# Install like base image
+RUN apt-get --no-install-recommends install -y gcc-12 python3.11 python3-pip python3-dev wget
+
+RUN apt-get -y update
+RUN apt-get -o DPkg::Options::="--force-confnew" -y dist-upgrade
+RUN apt-get --no-install-recommends install -y \
+     postgresql-12 postgresql-server-dev-12 \
+     postgresql-13 postgresql-server-dev-13 \
+     postgresql-14 postgresql-server-dev-14 \
+     postgresql-15 postgresql-server-dev-15 \
+     postgresql-16 postgresql-server-dev-16 \
+     postgresql-12-pg-track-settings postgresql-12-pg-wait-sampling postgresql-12-cron postgresql-12-set-user postgresql-12-pg-stat-kcache postgresql-12-pgaudit postgresql-12-pg-qualstats postgresql-12-hypopg postgresql-12-powa \
+     postgresql-13-pg-track-settings postgresql-13-pg-wait-sampling postgresql-13-cron postgresql-13-set-user postgresql-13-pg-stat-kcache postgresql-13-pgaudit postgresql-13-pg-qualstats postgresql-13-hypopg postgresql-13-powa \
+     postgresql-14-pg-track-settings postgresql-14-pg-wait-sampling postgresql-14-cron postgresql-14-set-user postgresql-14-postgis postgresql-14-pg-stat-kcache postgresql-14-pgaudit postgresql-14-pg-qualstats postgresql-14-hypopg postgresql-14-powa postgresql-14-pg-hint-plan postgresql-14-pgnodemx postgresql-14-decoderbufs \
+     postgresql-15-pg-track-settings postgresql-15-pg-wait-sampling postgresql-15-cron postgresql-15-set-user postgresql-15-postgis postgresql-15-pg-stat-kcache postgresql-15-pgaudit postgresql-15-pg-qualstats postgresql-15-hypopg postgresql-15-powa postgresql-15-pg-hint-plan postgresql-15-pgnodemx postgresql-15-decoderbufs \
+     postgresql-16-pg-track-settings postgresql-16-pg-wait-sampling postgresql-16-cron postgresql-16-set-user postgresql-16-postgis postgresql-16-pg-stat-kcache postgresql-16-pgaudit postgresql-16-pg-qualstats postgresql-16-hypopg postgresql-16-powa postgresql-16-pg-hint-plan postgresql-16-pgnodemx postgresql-16-decoderbufs
+
+RUN apt-get --no-install-recommends install -y libproj-dev libgdal30 libgeos3.10.2 libgeotiff5 libsfcgal1
+
+RUN localedef -i en_US -f UTF-8 en_US.UTF-8 && \
+    localedef -i es_PE -f UTF-8 es_PE.UTF-8 && \
+    localedef -i es_ES -f UTF-8 es_ES.UTF-8
+# Migrate .rpm to .deb
+RUN apt-get install -y alien
+
+RUN apt-get install -y protobuf-compiler
+
+WORKDIR /tmp
+
+COPY ./docker/start.sh /start.sh
+
+
+RUN chgrp 0 /var/lib/pgsql/ && chmod g+w /var/lib/pgsql/ && chmod 777 /var/lib/pgsql && \
+    chgrp 0 /var/run/postgresql/ && chmod g+w /var/run/postgresql/ && chmod 777 /var/run/postgresql && \
+    chmod -R 777 /etc/passwd && \
+    mv /usr/bin/python3.11 /usr/bin/python && ln -fs /usr/bin/python /usr/bin/python3 && \
+    chmod +x /start.sh
+
+
+RUN pg_config --version | grep -o "[0-9]*" | head -n 1
+VOLUME /var/lib/pgsql
+VOLUME /var/run/postgresql
+
+CMD ["/bin/bash", "/start.sh"]
+
+USER 26
---
+++ docker/start.sh
@@ -0,0 +1,241 @@
+#!/usr/bin/env bash
+# Copyright 2024-2025 NetCracker Technology Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+RETRIES=100
+SLEEP_BETWEEN_ITERATIONS=5
+
+[[ "${DEBUG}" == 'true' ]] && set -x
+
+function handle_master_upgrade() {
+    cd /var/lib/pgsql/data/
+    echo "[$(date +%Y-%m-%dT%H:%M:%S)] cur path: `pwd`"
+
+
+    DB_SIZE_GB_FLOAT=$(du -sk /var/lib/pgsql/data/${DATA_DIR} | awk '{ print $1 / 1024 / 1024 }')
+    DB_SIZE_GB=`printf "%.0f\n" ${DB_SIZE_GB_FLOAT}`
+    PV_SIZE_GB=$(echo "${PV_SIZE}" | tr -dc '0-9')
+
+    echo
+    echo '##########################'
+    echo "MIGRATION_PV_USED: $MIGRATION_PV_USED"
+    echo "CLEAN_MIGRATION_PV: $CLEAN_MIGRATION_PV"
+    echo "MIGRATION_PATH: $MIGRATION_PATH"
+    echo "INITDB_PARAMS: $INITDB_PARAMS"
+    echo "SIZE OF VOLUME: $PV_SIZE_GB Gb"
+    echo "SOURCE DB SIZE: $DB_SIZE_GB Gb"
+    echo "DATA_DIR: $DATA_DIR"
+    echo '##########################'
+    echo
+
+    # check if there is enough space in case when migration pv is used
+    if [[ "${MIGRATION_PV_USED}" =~ ^[Tt]rue$ ]]; then
+        echo "[$(date +%Y-%m-%dT%H:%M:%S)] Migration PV is used, check if there is enough space for migration in migration PV"
+        if [[ $DB_SIZE_GB -gt $PV_SIZE_GB ]]; then
+            echo "[$(date +%Y-%m-%dT%H:%M:%S)] DB size is more than PV size, exiting ..."
+            exit 1
+        fi
+        if [[ "${CLEAN_MIGRATION_PV}" =~ ^[Tt]rue$ ]]; then
+            echo "[$(date +%Y-%m-%dT%H:%M:%S)] CLEAN_MIGRATION_PV set to True, All data from MIGRATION PV will be deleted! "
+            rm -rf /var/lib/pgsql/tmp_data/*
+        fi
+    else
+        echo "[$(date +%Y-%m-%dT%H:%M:%S)] Migration PV is NOT used, check if there is enough space for migration in master PV"
+        DOUBLE_DB_SIZE="$((${DB_SIZE_GB} * 2))"
+        if [[ ${DOUBLE_DB_SIZE} -gt ${PV_SIZE_GB} ]]; then
+            echo "[$(date +%Y-%m-%dT%H:%M:%S)] DB size is more than PV size, exiting ..."
+            exit 1
+        fi
+    fi
+    export PATH="/usr/lib/postgresql/${PG_VERSION_TARGET}/bin:${PATH}"
+
+    [ -d "$MIGRATION_PATH/tmp/pg" ] && echo "[$(date +%Y-%m-%dT%H:%M:%S)] Prev upgrade dir exists, removing .." && rm -rf "$MIGRATION_PATH/tmp/pg"
+
+    echo "[$(date +%Y-%m-%dT%H:%M:%S)] initializing target db with parameters $INITDB_PARAMS"
+    /usr/lib/postgresql/"${PG_VERSION_TARGET}"/bin/initdb ${INITDB_PARAMS} --pgdata="$MIGRATION_PATH/tmp/pg"
+
+
+    echo "[$(date +%Y-%m-%dT%H:%M:%S)] initialize complete, copying configs"
+    mkdir "/tmp/configs/"
+    cp /var/lib/pgsql/data/${DATA_DIR}/*.conf "/tmp/configs/"
+
+    echo "turning off wal archiving"
+    sed -e '/archive_command/ s/^#*/#/' -i "$MIGRATION_PATH/tmp/pg/postgresql.conf"
+    sed -e '/archive_mode/ s/^#*/#/' -i "$MIGRATION_PATH/tmp/pg/postgresql.conf"
+    sed -e '/ssl/ s/^#*/#/' -i "$MIGRATION_PATH/tmp/pg/postgresql.conf"
+    sed -e '/archive_command/ s/^#*/#/' -i "/var/lib/pgsql/data/${DATA_DIR}/postgresql.conf"
+    sed -e '/archive_mode/ s/^#*/#/' -i "/var/lib/pgsql/data/${DATA_DIR}/postgresql.conf"
+    sed -e '/ssl/ s/^#*/#/' -i "/var/lib/pgsql/data/${DATA_DIR}/postgresql.conf"
+
+    echo "[$(date +%Y-%m-%dT%H:%M:%S)] proceed with upgrade"
+    echo "[$(date +%Y-%m-%dT%H:%M:%S)] Content of /tmp/configs:"
+    ls -la /tmp/configs/
+    echo "[$(date +%Y-%m-%dT%H:%M:%S)] Content of /tmp/configs/postgresql.conf file:"
+    cat /tmp/configs/postgresql.conf
+
+    echo "[$(date +%Y-%m-%dT%H:%M:%S)] making chmod 750 to datadir"
+
+    chmod 750 $MIGRATION_PATH/${DATA_DIR}
+
+    cat "/var/lib/pgsql/data/${DATA_DIR}/postgresql.conf" | grep shared_preload_libraries >> $MIGRATION_PATH/tmp/pg/postgresql.conf
+
+    ls -la $MIGRATION_PATH
+
+    echo "[$(date +%Y-%m-%dT%H:%M:%S)] Check cluster before upgrade"
+      /usr/lib/postgresql/"${PG_VERSION_TARGET}"/bin/pg_upgrade \
+      --old-datadir "/var/lib/pgsql/data/${DATA_DIR}" \
+      --new-datadir "$MIGRATION_PATH/tmp/pg" \
+      --old-bindir  "/usr/lib/postgresql/${PG_VERSION}/bin" \
+      --new-bindir  "/usr/lib/postgresql/${PG_VERSION_TARGET}/bin" \
+      --check \
+      > /var/lib/pgsql/data/check_result
+
+    CHECK_CODE=$?
+
+    if [[ "CHECK_CODE" -ne 0 ]]; then
+        echo "[$(date +%Y-%m-%dT%H:%M:%S)] Check cluster before upgrade - Failed."
+        echo "check exit code: ${CHECK_CODE}"
+        cat /var/lib/pgsql/data/check_result
+        exit 13
+    fi
+
+    if [[ "$OPERATOR" =~ ^[Tt]rue$ ]]; then
+      echo "[$(date +%Y-%m-%dT%H:%M:%S)] using link parameter"
+      /usr/lib/postgresql/"${PG_VERSION_TARGET}"/bin/pg_upgrade \
+      --link \
+      --old-datadir "/var/lib/pgsql/data/${DATA_DIR}" \
+      --new-datadir "$MIGRATION_PATH/tmp/pg" \
+      --old-bindir  "/usr/lib/postgresql/${PG_VERSION}/bin" \
+      --new-bindir  "/usr/lib/postgresql/${PG_VERSION_TARGET}/bin"
+    else
+      /usr/lib/postgresql/"${PG_VERSION_TARGET}"/bin/pg_upgrade \
+      --old-datadir "/var/lib/pgsql/data/${DATA_DIR}" \
+      --new-datadir "$MIGRATION_PATH/tmp/pg" \
+      --old-bindir  "/usr/lib/postgresql/${PG_VERSION}/bin" \
+      --new-bindir  "/usr/lib/postgresql/${PG_VERSION_TARGET}/bin"
+    fi
+
+    EXIT_CODE=$?
+    if [[ "$EXIT_CODE" -ne 0 ]]; then
+        echo "[$(date +%Y-%m-%dT%H:%M:%S)] Error! Can not proceed, because upgrade failed, exiting"
+        ls -al /var/lib/pgsql/data
+        echo "[$(date +%Y-%m-%dT%H:%M:%S)] Printing all of the logs:"
+        awk '{print}' /var/lib/pgsql/data/*.log
+#        cat pg_upgrade_server.log
+        exit 1
+    fi
+
+    echo "[$(date +%Y-%m-%dT%H:%M:%S)] Upgrade Successful"
+    echo "[$(date +%Y-%m-%dT%H:%M:%S)] Sizing After Upgrade"
+    du -sh /var/lib/pgsql/data/
+
+    rm -rf "/var/lib/pgsql/data/${DATA_DIR}"
+
+    echo "[$(date +%Y-%m-%dT%H:%M:%S)] moving new data to directory"
+    echo "[$(date +%Y-%m-%dT%H:%M:%S)] from -> $MIGRATION_PATH/tmp/pg"
+    echo "[$(date +%Y-%m-%dT%H:%M:%S)] to -> /var/lib/pgsql/data/${DATA_DIR}"
+    mv "$MIGRATION_PATH/tmp/pg" "/var/lib/pgsql/data/${DATA_DIR}"
+
+    /usr/lib/postgresql/"${PG_VERSION_TARGET}"/bin/pg_ctl start -D "/var/lib/pgsql/data/${DATA_DIR}"
+
+    until psql -c "select 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
+      echo "[$(date +%Y-%m-%dT%H:%M:%S)] Waiting for postgres server, $((RETRIES--)) remaining attempts..."
+      sleep ${SLEEP_BETWEEN_ITERATIONS}
+    done
+
+    EXIT_CODE=$?
+    if [[ "$EXIT_CODE" -ne 0 ]]; then
+        echo "[$(date +%Y-%m-%dT%H:%M:%S)] Error! Can not proceed, because select failed, exiting"
+        cat pg_upgrade_server.log
+        exit 1
+    fi
+
+    /usr/lib/postgresql/"${PG_VERSION_TARGET}"/bin/psql -c "DROP ROLE IF EXISTS replicator; CREATE ROLE replicator with replication login password '$PGREPLPASSWORD';"
+    /usr/lib/postgresql/"${PG_VERSION_TARGET}"/bin/psql -c "ALTER ROLE postgres with password '$PGPASSWORD';"
+
+    # update extensions
+    [ -f /var/lib/pgsql/data/update_extensions.sql ] && psql --username=postgres --file=/var/lib/pgsql/data/update_extensions.sql postgres
+
+    /usr/lib/postgresql/"${PG_VERSION_TARGET}"/bin/pg_ctl stop -D "/var/lib/pgsql/data/${DATA_DIR}"
+
+    [ -f /var/lib/pgsql/data/target_version ] && rm -rf /var/lib/pgsql/data/target_version
+
+    echo "[$(date +%Y-%m-%dT%H:%M:%S)] moving configs to directory"
+    mv /tmp/configs/* "/var/lib/pgsql/data/${DATA_DIR}/"
+    echo "[$(date +%Y-%m-%dT%H:%M:%S)] exiting"
+}
+
+function handle_replica_upgrade() {
+    rm -rf "/var/lib/pgsql/data/${DATA_DIR}/*"
+}
+
+# restricted scc
+function check_user(){
+    cur_user=$(id -u)
+    if [[ "$cur_user" != "26" ]]
+    then
+        echo "[$(date +%Y-%m-%dT%H:%M:%S)] starting as not postgres user"
+        set -e
+
+        echo "[$(date +%Y-%m-%dT%H:%M:%S)] Adding randomly generated uid to passwd file..."
+
+        sed -i '/postgres/d' /etc/passwd
+
+        if ! whoami &> /dev/null; then
+          if [[ -w /etc/passwd ]]; then
+            export USER_ID=$(id -u)
+            export GROUP_ID=$(id -g)
+            echo "postgres:x:${USER_ID}:${GROUP_ID}:PostgreSQL Server:${PGDATA}:/bin/bash" >> /etc/passwd
+            echo "UID added ..."
+          fi
+        fi
+
+    fi
+}
+
+function check_pgsql_version(){
+    echo "[$(date +%Y-%m-%dT%H:%M:%S)] => Check Source And Target PGSQL version"
+    # get version of data files
+    PG_VERSION=$(head -n 1 "/var/lib/pgsql/data/${DATA_DIR}/PG_VERSION")
+
+    if python -c "import sys; sys.exit(0 if 11.0 <= float("${PG_VERSION}") < 12.0 else 1)"; then
+        PG_VERSION_TARGET="12"
+    elif python -c "import sys; sys.exit(0 if 10.0 <= float("${PG_VERSION}") < 11.0 else 1)"; then
+        PG_VERSION_TARGET="11"
+    else
+        PG_VERSION_TARGET="10"
+    fi
+
+    for i in {1..10}; do
+      echo "[$(date +%Y-%m-%dT%H:%M:%S)] Will try to find target_version file"
+      [ -f /var/lib/pgsql/data/target_version ] && echo "Target file found, will use version from this file" && \
+        PG_VERSION_TARGET=`cat /var/lib/pgsql/data/target_version` && break
+      sleep 1
+    done
+
+    echo "[$(date +%Y-%m-%dT%H:%M:%S)] Target version of PostgreSQL is $PG_VERSION_TARGET"
+}
+
+check_user
+
+check_pgsql_version
+
+if [[ "${TYPE}" == "master" ]]; then
+    handle_master_upgrade
+elif [[ "${TYPE}" == "replica" ]]; then
+    handle_replica_upgrade
+fi
+
+exit 0
